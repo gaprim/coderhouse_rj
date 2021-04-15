32a1bfeaa7d9aff6ef18f7d1dde2049e709a15a4
Gabriel Primiani <gaprim@gmail.com>
2021-04-15 20:40:44 -0300

    Test

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import "./ItemList.css";
+import data from './data/data.js';
 
-const productos = [
-  { id: 1, title: "Producto A", price: 9.1, pictureUrl:'' },
-  { id: 2, title: "Producto B", price: 4.6, pictureUrl:'' },
-  { id: 3, title: "Producto C", price: 3.5, pictureUrl:'' },
-  { id: 4, title: "Producto D", price: 3.4, pictureUrl:'' },
-];
+// const productos = [
+//   { id: 1, title: "Producto A", price: 9.1, pictureUrl:'' },
+//   { id: 2, title: "Producto B", price: 4.6, pictureUrl:'' },
+//   { id: 3, title: "Producto C", price: 3.5, pictureUrl:'' },
+//   { id: 4, title: "Producto D", price: 3.4, pictureUrl:'' },
+// ];
 
 class ItemList extends Component {
   constructor() {
@@ -26,7 +27,7 @@ componentDidMount(){
     return <div>
         <ul className ='listaProd'>
             {this.state.state_productos.map((a) => {
-                return <li>{a.title} - {a.price}</li>
+                return <li>{a.name} - {a.price}</li>
             })}
         </ul>
     </div>;
---
+++ src/Components/testetet/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Producto 1",
+      image: "../public/images/img01.png",
+      price: 100,
+      description: "Este es el producto 1",
+    },
+    {
+        id: 2,
+        name: "Producto 2",
+        image: "../public/images/img02.png",
+        price: 120,
+        description: "Este es el producto ",
+      },
+      {
+        id: 3,
+        name: "Producto 3",
+        image: "../public/images/img03.png",
+        price: 130,
+        description: "Este es el producto 3",
+      },
+      {
+        id: 4,
+        name: "Producto 4",
+        image: "../public/images/img04.png",
+        price: 140,
+        description: "Este es el producto 4",
+      },
+      {
+        id: 5,
+        name: "Producto 5",
+        image: "../public/images/img05.png",
+        price: 150,
+        description: "Este es el producto 5",
+      },
+  ],
+};
